cafc19de4a8bdeb18c07adde8fb800f17ef84acd
skøgard <skogard@protonmail.com>
2022-08-22 00:14:19 -0400

    update

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  workspace: "workspace",
+  domain: {
+    "address": "0x760b5a896bb62424805feb53976864bed36da160",
+    "chainId": 4,
+    "name": "C0 Rinkeby Genesis"
+  },
+  key: "m'/44'/60'/0'/0/0",
+}
