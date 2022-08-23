86b12b547cad07d8b01e17b8ce1f0f8014956256
skøgard <skogard@protonmail.com>
2022-08-23 17:47:22 -0400

    update

--- demo/fs.js
+++ demo/fs.js
@@ -1,6 +1,6 @@
 const fetch = require("cross-fetch")
 const token = async () => {
-  let r = await fetch("https://micronuron.vercel.app/fs/connect").then((r) => {
+  let r = await fetch("https://mnx-skogard.vercel.app/fs/connect").then((r) => {
     return r.json()
   })
   console.log("r", r)
--- pages/api/fs/connect.js
+++ pages/api/fs/connect.js
@@ -1,4 +1,6 @@
 const auth = JSON.parse(process.env.AUTH)
+const Nextparty = require('nextparty')
+const party = new Nextparty({ secret: auth.secret })
 export default async function handler(req, res) {
   let error = await party.protect("admin", req, res)
   if (error) {
