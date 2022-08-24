f217002716e8c88425f31fef4117d3c157bdf1cb
skøgard <skogard@protonmail.com>
2022-08-24 01:36:13 -0400

    update

--- demo/fs.js
+++ demo/fs.js
@@ -1,8 +1,37 @@
-const fetch = require("cross-fetch")
+import fs from 'fs'
+import path from 'path'
+import { NFTStorage, Blob } from 'nft.storage'
+import { fetch } from 'undici'
+import { KeyPair } from 'ucan-storage/keypair';
+import { build, validate } from 'ucan-storage/ucan-storage';
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50IjoiMHhmYjdiMjcxN2Y3YTJhMzBiNDJlMjFjZWYwM2RkMGZjNzZlZjc2MWU5IiwiZXhwaXJlc0luIjoyNTkyMDAwLCJzdWIiOiIweGZiN2IyNzE3ZjdhMmEzMGI0MmUyMWNlZjAzZGQwZmM3NmVmNzYxZTkiLCJhdWQiOiJhZG1pbiIsImF1dGgiOnsibWVtYmVyIjp0cnVlfSwiaWF0IjoxNjYxMjkxNTAwfQ.oQzDzR-bI8H8apLqlOeQkYg1tBibexS0G_7S-0vooLk"
 const token = async () => {
-  let r = await fetch("https://mnx-skogard.vercel.app/fs/connect").then((r) => {
+  let r = await fetch("https://mx2.vercel.app/api/fs/connect", {
+    headers: {
+      "Content-Type": "application/json",
+      "authorization": "token " + jwt
+    },
+  }).then((r) => {
     return r.json()
   })
   console.log("r", r)
+
+
+  const kp = await KeyPair.fromExportedKey(r.key)
+  const auth = {
+    endpoint: "https://api.nft.storage",
+    token: r.token,
+    did: kp.did()
+  }
+
+  const storage = new NFTStorage(auth)
+
+  // 5. Initialize NFTStorage
+  // 6. Upload
+  const data = await fs.promises.readFile(path.resolve(".", "config.js"))
+  const cid = await storage.storeBlob(new Blob([data]))
+  console.log({ cid })
+  const check = await storage.check(cid)
+  console.log(check.pin)
 }
 token()
--- pages/api/token/sign.js
+++ pages/api/token/sign.js
@@ -1,12 +1,11 @@
 require('dotenv').config()
 const Nurond = require('../../../lib/nurond')
-const config = require('../../../config')
 const Nextparty = require('nextparty')
 const auth = JSON.parse(process.env.AUTH)
 const party = new Nextparty({ secret: auth.secret })
 const nuron = new Nurond()
 export default async function handler(req, res) {
-  await nuron.init(config)
+  await nuron.init()
   let error = await party.protect("admin", req, res)
   if (error) {
     return res.status(401).json(error)
